b2771df1753ed1c7dfb08e2dc9a4d69d90b4afeb
Suhail Ahmed Toha <2203038@student.ruet.ac.bd>
2025-03-12 14:11:08 +0600

    Fixed base URL configuration

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/Cursor-Portfolio/",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+  },
 });
